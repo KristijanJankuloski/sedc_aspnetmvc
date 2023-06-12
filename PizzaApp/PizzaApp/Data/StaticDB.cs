61cdbc923f0891dd4b0562155eac47647da3b05d
Kristijan <jankuloski.k@gmail.com>
2023-06-12 16:30:15 +0200

    Homework 1 done

--- PizzaApp/PizzaApp/Data/StaticDB.cs
+++ PizzaApp/PizzaApp/Data/StaticDB.cs
@@ -9,5 +9,10 @@ namespace PizzaApp.Data
             new Pizza(){ Id = 1, Name = "Capri", Price = 12.99, IsOnPromotion = true },
             new Pizza(){ Id = 2, Name = "Peperoni", Price = 11.99, IsOnPromotion = false }
         };
+        public static List<Order> Orders = new List<Order>()
+        {
+            new Order(1, "John Doe", new List<Pizza>(){ Pizzas[0] }),
+            new Order(2, "Bob Bobski", new List<Pizza>() { Pizzas[0], Pizzas[1] })
+        };
     }
 }
